1f2a9de518abbd5599aa685fb70e14472b6af916
Minor Caster <71119238+minorcaster@users.noreply.github.com>
2023-06-06 17:56:45 +0800

    Add files via upload

--- api/transcribe.js
+++ api/transcribe.js
@@ -1,41 +1,46 @@
-// /api/transcribe.js
-const FormData = require('form-data');
 const fetch = require('node-fetch');
+const FormData = require('form-data');
+const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
 
-module.exports = async (req, res) => {
-  const apiKey = process.env.OPENAI_API_KEY;
-
-  // Extract parameters from the request
-  const file = req.body.file;
-  const language = req.body.language;
-  const response_format = req.body.response_format || 'verbose_json';
-
-  // Prepare the form data
-  const formData = new FormData();
-  formData.append('file', file);
-  formData.append('model', 'whisper-1');
-  formData.append('response_format', response_format);
-  if (language) {
-    formData.append('language', language);
-  }
-
-  // Make the request to the OpenAI API
-  const response = await fetch('https://api.openai.com/v1/audio/transcriptions', {
-    method: 'POST',
-    body: formData,
-    headers: {
-      'Authorization': `Bearer ${apiKey}`
+module.exports = (req, res) => {
+  upload.single('file')(req, res, async (err) => {
+    if (err) {
+      return res.status(400).json({ error: 'Error processing file upload.' });
     }
+
+    const apiKey = process.env.OPENAI_API_KEY;
+    const file = req.file;  // The uploaded file
+    const language = req.body.language;
+    const response_format = req.body.response_format || 'verbose_json';
+
+    // Prepare the form data
+    const formData = new FormData();
+    formData.append('file', file.buffer, { filename: file.originalname });
+    formData.append('model', 'whisper-1');
+    formData.append('response_format', response_format);
+    if (language) {
+      formData.append('language', language);
+    }
+
+    // Make the request to the OpenAI API
+    const response = await fetch('https://api.openai.com/v1/audio/transcriptions', {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'Authorization': `Bearer ${apiKey}`
+      }
+    });
+
+    // Handle the response format
+    let result;
+    if (response_format === 'json' || response_format === 'verbose_json') {
+      result = await response.json();
+    } else {
+      result = await response.text();
+    }
+
+    // Return the result
+    res.json({ result });
   });
-
-  // Handle the response format
-  let result;
-  if (response_format === 'json' || response_format === 'verbose_json') {
-    result = await response.json();
-  } else {
-    result = await response.text();
-  }
-
-  // Return the result
-  res.json(result);
 };
